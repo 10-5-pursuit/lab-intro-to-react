049cc678d0186114632cb56a00c9e189a16781bd
mquispe96 <mquispe@pursuit.org>
2024-04-04 18:41:38 -0400

    Start React Lab, do not have to finish

---
+++ src/Components/Post.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Post = () => {
+    return (
+        <div className="post">
+            <h5>Title</h5>
+            <p>Lorem Ipsem</p>
+        </div>
+    )
+}
+
+export default Post;
