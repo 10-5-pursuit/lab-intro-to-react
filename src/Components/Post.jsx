f5731e10c0991ccc2ce8f307ec543d39c39281d3
sadya104 <sandrickson@pursuit.com>
2024-04-15 04:08:01 -0400

    finished

---
+++ src/Components/Post.jsx
@@ -0,0 +1,13 @@
+/// Post.jsx 
+
+import './Post.scss'
+
+export default function Post (){
+
+    return (
+        <div className="post">
+            <h3 className="post__title">Title</h3>
+            <p className="post__text">Lorem Ipsum</p>
+        </div>
+    )
+}
